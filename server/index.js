9fed199a4db9214048bf91acce74fbc11e489593
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-04 15:45:38 +0530

    Add Postgres Client configuration details

--- server/index.js
+++ server/index.js
@@ -8,3 +8,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+
+// POSTGRES CLIENT SETUP
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
