96e04dd09e919c788c4351e70286dd5f3e595d13
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-09 23:32:36 +0530

    Configure deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
-docker build -t "$DOCKER_USERNAME"/complex-fibonacci-client -f ./client/Dockerfile ./client
-docker build -t "$DOCKER_USERNAME"/complex-fibonacci-server -f ./server/Dockerfile ./server
-docker build -t "$DOCKER_USERNAME"/complex-fibonacci-worker -f ./worker/Dockerfile ./worker
+docker build -t $DOCKER_USERNAME/complex-fibonacci-client:latest -t $DOCKER_USERNAME/complex-fibonacci-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t $DOCKER_USERNAME/complex-fibonacci-server:latest -t $DOCKER_USERNAME/complex-fibonacci-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t $DOCKER_USERNAME/complex-fibonacci-worker:latest -t $DOCKER_USERNAME/complex-fibonacci-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
-docker push "$DOCKER_USERNAME"/complex-fibonacci-client
-docker push "$DOCKER_USERNAME"/complex-fibonacci-server
-docker push "$DOCKER_USERNAME"/complex-fibonacci-worker
+docker push $DOCKER_USERNAME/complex-fibonacci-client:latest
+docker push $DOCKER_USERNAME/complex-fibonacci-server:latest
+docker push $DOCKER_USERNAME/complex-fibonacci-worker:latest
+
+docker push $DOCKER_USERNAME/complex-fibonacci-client:$GIT_SHA
+docker push $DOCKER_USERNAME/complex-fibonacci-server:$GIT_SHA
+docker push $DOCKER_USERNAME/complex-fibonacci-worker:$GIT_SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment client=$DOCKER_USERNAME/complex-fibonacci-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=$DOCKER_USERNAME/complex-fibonacci-server:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=$DOCKER_USERNAME/complex-fibonacci-worker:$GIT_SHA
